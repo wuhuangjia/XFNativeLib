9ab38d6ec903f2597873c934c038b344bc41e392
wuhuangjia <wuhuangjia@msn.com>
2017-10-27 00:58:44 +0800

    Add project files.

---
+++ XFNativeLib/XFNativeLib/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XFNativeLib.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
